951040976d58ecfde0bd849b6459f4384f6b0192
ziaulh53 <ziaul.siu@gmail.com>
2023-09-12 23:43:52 +0600

    dd

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\LogoutRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function  signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password']),
+        ]);
+
+        $token =  $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+    public function  login(LoginRequest $request)
+    {
+
+        $credential = $request->validated();
+        if (!Auth::attempt($credential)) {
+            return response([
+                'message' => 'Email or Password is incorrect',
+                'success' => false
+            ], 422);
+        }
+        /** @var User $user */
+        $user  = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        $success = true;
+        return response(compact('user', 'token', 'success'));
+    }
+    public function  logout(LogoutRequest $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete;
+        return response(['success'=>true]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/csrf-cookie', function () {
+    return response()->json(['message' => 'CSRF token acquired']);
+});
+
